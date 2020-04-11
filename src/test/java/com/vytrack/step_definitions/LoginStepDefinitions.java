e82ea78bfc4edab92106c9beae1ce58adf59818b
Vasyl Fomiuk <vasyl@cybertekschool.com>
2020-04-11 16:54:24 -0400

    Added login feature file

--- src/test/java/com/vytrack/step_definitions/LoginStepDefinitions.java
+++ src/test/java/com/vytrack/step_definitions/LoginStepDefinitions.java
@@ -1,4 +1,23 @@
 package com.vytrack.step_definitions;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class LoginStepDefinitions {
+
+    @Given("user is on the login page")
+    public void user_is_on_the_login_page() {
+
+    }
+
+    @When("user logs in as a sales manager")
+    public void user_logs_in_as_a_sales_manager() {
+
+    }
+
+    @Then("user should verify that title is a Dashboard")
+    public void user_should_verify_that_title_is_a_Dashboard() {
+
+    }
 }
