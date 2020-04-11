2ee88cdc0cecec9a5ee5716920e9734854f61e34
Vasyl Fomiuk <vasyl@cybertekschool.com>
2020-04-11 17:29:47 -0400

    Added login feature file

--- src/test/java/com/vytrack/step_definitions/LoginStepDefinitions.java
+++ src/test/java/com/vytrack/step_definitions/LoginStepDefinitions.java
@@ -1,23 +1,36 @@
 package com.vytrack.step_definitions;
 
+import com.vytrack.pages.LoginPage;
+import com.vytrack.utilities.ConfigurationReader;
+import com.vytrack.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 
 public class LoginStepDefinitions {
+    LoginPage loginPage = new LoginPage();
 
     @Given("user is on the login page")
     public void user_is_on_the_login_page() {
+        System.out.println("Open login page");
 
+        String URL = ConfigurationReader.getProperty("qa3");
+        Driver.getDriver().get(URL);
     }
 
     @When("user logs in as a sales manager")
     public void user_logs_in_as_a_sales_manager() {
+        System.out.println("Login as sales manager");
 
+        loginPage.login();
     }
 
     @Then("user should verify that title is a Dashboard")
     public void user_should_verify_that_title_is_a_Dashboard() {
+        System.out.println("Verify that title is a Dashboard");
 
+        Assert.assertEquals("Dashboard", Driver.getDriver().getTitle());
+        Driver.closeDriver();
     }
 }
