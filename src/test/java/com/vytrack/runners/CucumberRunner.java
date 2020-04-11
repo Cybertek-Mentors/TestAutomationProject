f7da96de9cff7eeca9be9c0f9d248091bd8fa22f
Vasyl Fomiuk <vasyl@cybertekschool.com>
2020-04-11 15:30:33 -0400

    added Runner class

---
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -0,0 +1,15 @@
+package com.vytrack.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        glue = "com/vytrack/step_definitions",
+        features = "src/test/resources/features"
+)
+public class CucumberRunner {
+
+
+}
