289ff4002de6b27dad119776d11c48c697025988
Vasyl Fomiuk <vasyl@cybertekschool.com>
2020-04-15 20:32:23 -0400

    Cucumber tags

--- src/test/java/com/vytrack/runners/CucumberRunner.java
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         dryRun = false,
         strict = false,
-        tags = "@driver"
+        tags = "not @driver"
 )
 public class CucumberRunner {
 
