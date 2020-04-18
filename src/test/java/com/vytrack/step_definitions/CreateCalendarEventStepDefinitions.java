f3baed7f46a7ea67da7154fe97721e5521003237
Vasyl Fomiuk <vasyl@cybertekschool.com>
2020-04-18 13:29:45 -0400

    created new feature file: CreateCalendarEvent.feature

--- src/test/java/com/vytrack/step_definitions/CreateCalendarEventStepDefinitions.java
+++ src/test/java/com/vytrack/step_definitions/CreateCalendarEventStepDefinitions.java
@@ -11,4 +11,22 @@ public class CreateCalendarEventStepDefinitions {
         System.out.println("User clicks on create calendar event button");
         calendarEventsPage.clickToCreateCalendarEvent();
     }
+
+    @Then("user enters {string} as a title")
+    public void user_enters_as_a_title(String string) {
+        System.out.println("User enters title: " + string);
+        calendarEventsPage.enterCalendarEventTitle(string);
+    }
+
+    @Then("user enters {string} as a description")
+    public void user_enters_as_a_description(String string) {
+        System.out.println("User enters description: " + string);
+        calendarEventsPage.enterCalendarEventDescription(string);
+    }
+
+    @Then("user click on save and close button")
+    public void user_click_on_save_and_close_button() {
+        System.out.println("User click on save and close button");
+        calendarEventsPage.clickOnSaveAndClose();
+    }
 }
--- src/test/resources/features/CreateCalendarEvent.feature
+++ src/test/resources/features/CreateCalendarEvent.feature
@@ -7,3 +7,6 @@ Feature: Create calendar event
     When user logs in as a sales manager
     And user navigates to "Activities" and "Calendar Events"
     Then user clicks on create calendar event button
+    And user enters "Sprint Retrospective" as a title
+    And user enters "On this meeting we discuss what went well, what went wrong and what can be improved" as a description
+    Then user click on save and close button
