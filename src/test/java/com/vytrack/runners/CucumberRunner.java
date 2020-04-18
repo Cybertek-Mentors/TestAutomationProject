928fec4ce00bb4aa5fe6a98bf4e5482fc4fbbb88
Vasyl Fomiuk <vasyl@cybertekschool.com>
2020-04-18 15:20:09 -0400

    maven cucumber html report

--- src/test/java/com/vytrack/runners/CucumberRunner.java
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -8,10 +8,13 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         glue = "com/vytrack/step_definitions",
         features = "src/test/resources/features",
-        dryRun = true,
+        dryRun = false,
         strict = false,
         tags = "@activities",
-        plugin = "html:target/cucumber-html-report"
+        plugin = {
+                "html:target/cucumber-html-report",
+                "json:target/cucumber.json"
+        }
 
 )
 public class CucumberRunner {
