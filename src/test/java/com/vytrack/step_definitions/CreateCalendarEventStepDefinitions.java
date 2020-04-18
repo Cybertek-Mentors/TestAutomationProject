497f34ba39b8fb88fc6fd411e3d6beb3dbecdf67
Vasyl Fomiuk <vasyl@cybertekschool.com>
2020-04-18 17:37:55 -0400

    maven cucumber html report

--- src/test/java/com/vytrack/step_definitions/CreateCalendarEventStepDefinitions.java
+++ src/test/java/com/vytrack/step_definitions/CreateCalendarEventStepDefinitions.java
@@ -42,15 +42,21 @@ public class CreateCalendarEventStepDefinitions {
     public void user_verifies_that_title_of_new_calendar_event_is(String string) {
         Assert.assertEquals(string, calendarEventsPage.getGeneralInfoTitleText());
     }
+//    And user enters new calendar event information:
+//            | description | On this meeting we discuss what went well, what went wrong and what can be improved |
+//            | title       | Sprint Retrospective                                                                |
 
     @Then("user enters new calendar event information:")
     public void user_enters_new_calendar_event_information(Map<String, String> dataTable) {
+        calendarEventsPage.enterCalendarEventDescription(dataTable.get("description"));
+        calendarEventsPage.enterCalendarEventTitle(dataTable.get("title"));
 
     }
 
     @Then("user verifies new calendar event was created successfully")
     public void user_verifies_new_calendar_event_was_created_successfully(Map<String, String> dataTable) {
-
+        Assert.assertEquals(dataTable.get("description"), calendarEventsPage.getGeneralInfoDescriptionText());
+        Assert.assertEquals(dataTable.get("title"), calendarEventsPage.getGeneralInfoTitleText());
     }
 
 }
