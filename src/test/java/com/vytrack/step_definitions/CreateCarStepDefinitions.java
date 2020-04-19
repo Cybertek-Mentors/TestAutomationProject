1cbfb9870d8a4968deb791dac8958b1f0c1ec7b5
Vasyl Fomiuk <vasyl@cybertekschool.com>
2020-04-19 16:52:00 -0400

    create a car feature

--- src/test/java/com/vytrack/step_definitions/CreateCarStepDefinitions.java
+++ src/test/java/com/vytrack/step_definitions/CreateCarStepDefinitions.java
@@ -20,6 +20,11 @@ public class CreateCarStepDefinitions {
 //            | SDET          | Pro Racer | Rome, Italy | 2020       | Red   |
     @Then("user creates a car with following info:")
     public void user_creates_a_car_with_following_info(List<Map<String, String>> dataTable) {
-
+        System.out.println(dataTable);
+        vehiclesPage.setLicencePlateInput(dataTable.get(0).get("License Plate"));
+        vehiclesPage.setDriverInput(dataTable.get(0).get("Driver"));
+        vehiclesPage.setLocationInput(dataTable.get(0).get("Location"));
+        vehiclesPage.setModelYear(dataTable.get(0).get("Model Year"));
+        vehiclesPage.setColor(dataTable.get(0).get("Color"));
     }
 }
