8e743189bacc75c675951b5a3692b2a01427fbcf
Vasyl Fomiuk <vasyl@cybertekschool.com>
2020-04-19 13:00:07 -0400

    added view calendar events feature file

---
+++ src/test/java/com/vytrack/runners/ActivitiesTestRunner.java
@@ -0,0 +1,22 @@
+package com.vytrack.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        glue = "com/vytrack/step_definitions",
+        features = "src/test/resources/features/activities",
+        dryRun = false,
+        strict = false,
+        tags = "",
+        plugin = {
+                "html:target/default-report",
+                "json:target/cucumber.json"
+        }
+
+)
+public class ActivitiesTestRunner {
+
+}
