67584ec9933fc69fdf35435ec83d51caf20334b4
Vasyl Fomiuk <vasyl@cybertekschool.com>
2020-04-19 16:39:33 -0400

    create a car feature

--- src/test/java/com/vytrack/runners/ActivitiesTestRunner.java
+++ src/test/java/com/vytrack/runners/ActivitiesTestRunner.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features/activities",
         dryRun = false,
         strict = false,
-        tags = "",
+        tags = "A",
         plugin = {
                 "html:target/default-report",
                 "json:target/cucumber2.json"
