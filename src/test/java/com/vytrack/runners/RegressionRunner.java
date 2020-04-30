73802bfb3ffacffcc4a2e0edb8176842df58dff5
Vasyl Fomiuk <vasyl@cybertekschool.com>
2020-04-29 20:51:37 -0400

    smoke test

--- src/test/java/com/vytrack/runners/RegressionRunner.java
+++ src/test/java/com/vytrack/runners/RegressionRunner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         plugin = {
                 "html:target/default-report-for-regression",
                 "json:target/cucumber_regression.json",
-                "rerun:target/rerun.txt"
+                "rerun:target/rerun_regression.txt"
         }
 
 )
